a7cc6f177a9d62ff9ccfa98690a4c0f74a18ac7b
Peter Koenders <pkoenders@gmail.com>
2020-05-23 02:55:46 +1200

    testimonials

---
+++ src/components/testimonial.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from 'gatsby-image'
+import aboutStyles from '../pages/about.module.scss';
+
+
+const Testimonial = () => {
+
+    let linkToItem
+
+    const data = useStaticQuery(graphql`
+    query {
+      allDatoCmsTestimonial(sort: { fields: [position], order: ASC }) {
+      edges {
+        node {
+          chooseColour {
+            hex
+          }
+          dateOfTestmonial(difference: "", formatString: "MMMM YYYY", fromNow: false, locale: "")
+          name
+          testmonial
+          linkToGalleryItem {
+            slug
+          }
+          addArtwork {
+            url
+            alt
+            fluid(
+              maxWidth: 120
+              imgixParams: { fm: "jpg", auto: "compress" }
+            ) {
+              ...GatsbyDatoCmsFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`)
+
+    return (
+        <>
+            {data.allDatoCmsTestimonial.edges.map((edge, i) => {
+
+                linkToItem = edge.node.linkToGalleryItem
+
+
+                if (linkToItem === null) {
+                    return (
+                        <div className={aboutStyles.testimonial} style={{ color: edge.node.chooseColour.hex }}>
+                            {edge.node.addArtwork &&
+                                <div className={aboutStyles.testimonialImgWrapper}>
+                                    <Img
+                                        fluid={edge.node.addArtwork.fluid}
+                                        alt={edge.node.addArtwork.alt}
+                                        src={edge.node.addArtwork.url}
+                                    ></Img>
+                                </div>
+                            }
+
+                            <p className={aboutStyles.testimonialQuote}>
+                                {edge.node.testmonial}&nbsp;
+                            </p>
+                            <p><strong>{edge.node.name}</strong> - <span>{edge.node.dateOfTestmonial}</span></p>
+                        </div>
+                    )
+                }
+
+                if (linkToItem != null) {
+                    return (
+
+                        <div className={aboutStyles.testimonial} style={{ color: edge.node.chooseColour.hex }}>
+
+                            <Link
+                                to={`/gallery/${linkToItem.slug}`}
+                                title={linkToItem.title}
+                            >
+                                {edge.node.addArtwork &&
+                                    <div className={aboutStyles.testimonialImgWrapper}>
+                                        <Img
+                                            fluid={edge.node.addArtwork.fluid}
+                                            alt={edge.node.addArtwork.alt}
+                                            src={edge.node.addArtwork.url}
+                                        ></Img>
+                                    </div>
+                                }
+
+                                <p className={aboutStyles.testimonialQuote}>
+                                    {edge.node.testmonial}&nbsp;
+                                </p>
+                                <p><strong>{edge.node.name}</strong> - <span>{edge.node.dateOfTestmonial}</span></p>
+                            </Link>
+
+                        </div>
+                    )
+
+                }
+
+            })}
+        </ >
+    )
+
+}
+
+export default Testimonial
--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,72 +1,33 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
-import Img from 'gatsby-image'
-import IndexBGroundImg from '../components/index-bground-img'
+import { useStaticQuery, graphql, Link } from "gatsby"
+//import Img from 'gatsby-image'
 import Layout from '../components/layout'
-
+import Testimonial from '../components/testimonial'
 import Head from '../components/head'
 import Header from "../components/header"
 import aboutStyles from './about.module.scss';
 
 
 
-
-
-
-
-const AboutPage = () => {
-
-
-
-  const data = useStaticQuery(graphql`
-query {
-      allDatoCmsTestimonial(sort: { fields: [position], order: ASC }) {
-      edges {
-        node {
-          chooseColour {
-            hex
-          }
-          dateOfTestmonial(difference: "", formatString: "MMMM YYYY", fromNow: false, locale: "")
-          name
-          testmonial
-        }
-      }
-    }
-  }
-`)
-
+const TestimonialPage = () => {
 
   return (
-    <div>
-      <IndexBGroundImg />
+    <>
       <Header />
       <Layout>
         <Head title="About Lynn Pronk" />
-
 
         <div className={aboutStyles.aboutWrapper}>
           <h2>Testimonials</h2>
           <div className={aboutStyles.aboutContent}>
 
-
-
-            {data.allDatoCmsTestimonial.edges.map((edge, i) => {
-
-              return (
-                <div style={{ color: edge.node.chooseColour.hex }}>
-                  <span>“</span><div dangerouslySetInnerHTML={{ __html: edge.node.testmonial }} /><span>”</span>
-
-                  <p>{edge.node.name}</p>
-                  <p>{edge.node.dateOfTestmonial}</p>
-                </div>
-              )
-            })}
+            <Testimonial />
 
           </div>
         </div>
       </Layout >
-    </div >
+    </>
   )
 }
 
-export default AboutPage
+export default TestimonialPage
