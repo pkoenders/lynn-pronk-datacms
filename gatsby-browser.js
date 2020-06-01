2730756611658e73d9826c885355b7c710f56b7c
Peter Koenders <pkoenders@gmail.com>
2020-06-02 10:26:44 +1200

    aa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import mediumZoom from 'medium-zoom'
 export const onRouteUpdate = () => {
     //mediumZoom('.medium-zoom-image div img')
-    mediumZoom('.medium-zoom-image div img')
+    mediumZoom('.medium-zoom-image picture img')
 
     // 'figure.zoom-effect'
 
@@ -29,7 +29,7 @@ export const onRouteUpdate = () => {
 function ready() {
     //alert('DOM is ready');
 
-    mediumZoom('.medium-zoom-image div img')
+    mediumZoom('.medium-zoom-image picture img')
     resizeAllGridItems()
     hoverGridItems()
     mobileNav()
