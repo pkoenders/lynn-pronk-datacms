43984d1a3a7935132a9fbae943ac4453714fdaae
Peter Koenders <pkoenders@gmail.com>
2020-08-08 00:37:04 +1200

    Update gallery.js

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -75,7 +75,7 @@ function Gallery({ categoryFilter }) {
             coverImage {
               url
               alt
-              fluid(maxWidth: 680, imgixParams: { fm: "webp", q: 50 }) {
+              fluid(maxWidth: 414, imgixParams: { fm: "webp", q: 50 }) {
                 ...GatsbyDatoCmsFluid
               }
             }
