dc2a7592778c49dcaf8ac06c66a58fcf9b35743e
Peter Koenders <pkoenders@gmail.com>
2020-08-21 02:25:57 +1200

    aa

--- src/templates/gallery-item.js
+++ src/templates/gallery-item.js
@@ -79,7 +79,7 @@ const PortfolioPageItem = ({ data, pageContext }) => {
                 alt={next.coverImage.alt}
                 //src={next.coverImage.url + '?auto=format&w=80'}
                 //src={next.coverImage.url}
-                fixed={next.coverImage.fluid}
+                fixed={next.coverImage.fixed}
                 //width='40px'
                 //height='auto'
                 loading="auto"
